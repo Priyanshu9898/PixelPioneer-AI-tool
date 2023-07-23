4aeb4288fe36e71e293dbfb335605ac326930753
Priyanshu9898 <priyanshumalaviya17@gmail.com>
2023-07-23 22:42:07 +0530

    some ui fix in register page

--- frontend/app/register/page.tsx
+++ frontend/app/register/page.tsx
@@ -4,7 +4,7 @@ import React from 'react'
 const Register = () => {
   return (
     <>
-        <section className="bg-gray-50 dark:bg-gray-900">
+        <section className="bg-gray-50 dark:bg-gray-900 min-h-screen">
   <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
       <Link href="#" className="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white">
           <img className="w-8 h-8 mr-2" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/logo.svg" alt="logo" />
