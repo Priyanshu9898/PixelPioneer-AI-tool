4058c80c65f8bd5a45d7e200c0f884fa9b357224
Priyanshu9898 <priyanshumalaviya17@gmail.com>
2023-07-23 16:15:24 +0530

    Navbar added

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,4 +1,4 @@
-
+import Navbar from "@/components/Navbar";
 import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
@@ -18,8 +18,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        {children}
         <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.7.0/flowbite.min.js"></script>
+        <Navbar />
+        {children}
       </body>
     </html>
   );
