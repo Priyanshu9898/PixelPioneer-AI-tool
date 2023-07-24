85a14408bf887156bbe349a847a6871be76493df
Priyanshu9898 <priyanshumalaviya17@gmail.com>
2023-07-24 11:03:52 +0530

    AuthContext added

---
+++ frontend/Context/AuthContext.tsx
@@ -0,0 +1,126 @@
+"use client";
+
+
+import { User } from "@/types/Auth";
+import { toastStyle } from "@/utils/ToastStyle";
+import axios from "axios";
+import Cookies from "js-cookie";
+import { useRouter } from "next/navigation";
+import React, {
+  useContext,
+  useState,
+  createContext,
+  ReactNode,
+  useEffect,
+} from "react";
+import { toast } from "react-hot-toast";
+
+
+type AuthContextData = {
+  user: User | null;
+  login: (
+    email: string,
+    password: string,
+    setIsLoading: (arg: boolean) => void
+  ) => void;
+  logout: () => Promise<void>;
+};
+
+const AuthContext = createContext<AuthContextData | undefined>(undefined);
+
+
+type AuthProviderProps = {
+  children: ReactNode;
+};
+
+export const AuthProvider = ({ children }: AuthProviderProps) => {
+  const [user, setUser] = useState<User | null>(null);
+
+  const router = useRouter();
+
+  useEffect(() => {
+    const fetchUser = async () => {
+
+      const storedUser = sessionStorage.getItem('user');
+      // If there is stored user data, update our state with it
+      if (storedUser !== null) {
+
+        console.log(JSON.parse(storedUser));
+        setUser(JSON.parse(storedUser));
+      }
+    };
+
+    fetchUser();
+  }, []);
+  
+
+  const login = async (
+    email: any,
+    password: any,
+    setIsLoading: (arg0: boolean) => void
+  ) => {
+    try {
+      setIsLoading(true);
+      const response = await axios.post("/api/login", {
+        email: email,
+        password: password,
+      });
+
+      toast.success(`${response.data?.message}`, {
+        style: toastStyle,
+      });
+
+      // console.log(response.data);
+
+
+      setUser(response.data.user);
+
+      sessionStorage.setItem('user', JSON.stringify(response.data.user));
+      sessionStorage.setItem('token', JSON.stringify(response.data.token));
+      
+      console.log("heyy", response.data.user)
+      
+      setIsLoading(false);
+
+      router.push("/");
+      router.refresh();
+
+    } catch (err) {
+      toast.error("Invalid Credentials!", {
+        style: toastStyle,
+      });
+      setUser(null);
+      setIsLoading(false);
+    }
+  };
+
+  const logout = async () => {
+    try {
+      const response = await axios.get("/api/logout");
+      console.log(response.data);
+      setUser(null);
+      sessionStorage.removeItem('user');
+      sessionStorage.removeItem('token');
+      toast.success("Logged Out Successfully!", { style: toastStyle });
+      router.push("/login");
+    } catch (err) {
+      toast.error("Uh oh! Something went wrong.", { style: toastStyle });
+    }
+  };
+
+  return (
+    <AuthContext.Provider value={{ user, login, logout }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export const useAuth = () => {
+  const context = React.useContext(AuthContext);
+
+  if (context === undefined) {
+    throw new Error("useAuth must be used within an AuthProvider");
+  }
+
+  return context;
+};
--- frontend/components/NavbarWithSidebar.tsx
+++ frontend/components/NavbarWithSidebar.tsx
@@ -271,10 +271,20 @@ const NavbarWithSidebar = () => {
                   href="/"
                   className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group"
                 >
-                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-  <path strokeLinecap="round" strokeLinejoin="round" d="M9 9l10.5-3m0 6.553v3.75a2.25 2.25 0 01-1.632 2.163l-1.32.377a1.803 1.803 0 11-.99-3.467l2.31-.66a2.25 2.25 0 001.632-2.163zm0 0V2.25L9 5.25v10.303m0 0v3.75a2.25 2.25 0 01-1.632 2.163l-1.32.377a1.803 1.803 0 01-.99-3.467l2.31-.66A2.25 2.25 0 009 15.553z" />
-</svg>
-
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth={1.5}
+                    stroke="currentColor"
+                    className="w-6 h-6"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M9 9l10.5-3m0 6.553v3.75a2.25 2.25 0 01-1.632 2.163l-1.32.377a1.803 1.803 0 11-.99-3.467l2.31-.66a2.25 2.25 0 001.632-2.163zm0 0V2.25L9 5.25v10.303m0 0v3.75a2.25 2.25 0 01-1.632 2.163l-1.32.377a1.803 1.803 0 01-.99-3.467l2.31-.66A2.25 2.25 0 009 15.553z"
+                    />
+                  </svg>
 
                   <span className="flex-1 ml-3 whitespace-nowrap">
                     Music Generator
